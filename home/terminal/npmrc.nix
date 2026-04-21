15d43d2decf0b6896cd637b0265e799df9dc17e9
GalvinGao <me@galvingao.com>
2026-04-21 17:34:46 +0900

    docs(npmrc): note PAT rotation command in module header

--- home/terminal/npmrc.nix
+++ home/terminal/npmrc.nix
@@ -5,6 +5,10 @@
   # Token comes from $NPM_GITHUB_TOKEN (set in zsh.nix via macOS keychain);
   # ''${...} escapes Nix interpolation so the literal ${NPM_GITHUB_TOKEN}
   # reaches .npmrc for npm/pnpm to expand at install time.
+  #
+  # Rotate the PAT (when it expires or scope changes):
+  #   security add-generic-password -U -a "$USER" -s npm-github-token \
+  #     -w "$(op read 'op://Personal/f7sgqmjdowtmdk2b7cvkyhwoka/password')"
   home.file.".npmrc".text = ''
     @troph-team:registry=https://npm.pkg.github.com
     //npm.pkg.github.com/:_authToken=''${NPM_GITHUB_TOKEN}
